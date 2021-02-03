d8a3a047f8209e33c50d11db3b5042b2c2b314cc
WeiShuaibing <1770666496@qq.com>
2021-02-03 18:37:45 +0800

    新增用户拼单接口

--- src/main/java/com/bishe/travel/controller/OrderController.java
+++ src/main/java/com/bishe/travel/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.bishe.travel.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.bishe.travel.entity.Order;
 import com.bishe.travel.entity.R;
 import com.bishe.travel.entity.Scenic;
 import com.bishe.travel.service.OrderService;
@@ -21,9 +22,52 @@ public class OrderController {
 
     @Autowired
     private OrderService service;
+    @Autowired
+    private ScenicService scenicService;
 
     @GetMapping("/userPintuan")
-    public R userPintuan() {
+    public R userPintuan(@RequestBody Order order) {
+        System.out.println(order);
+        /**
+         * 思路：传过来 用户id 景点id 以及要拼团的类型,
+         * order表 保存拼单的用户
+         * 从order表计算拼单的用户数
+         * 修改景点表中用户拼单的数量
+         */
+        service.save(order);// 保存订单信息
+
+        // 查询拼单数量
+        QueryWrapper<Order> query = Wrappers.<Order>query();
+        query.eq("status", 0).eq("type", order.getType());
+        int count = service.count(query);
+
+        Scenic scenic = scenicService.getById(order.getScenicId());
+        String pindanNum = scenic.getPindanNum();
+        char[] pindanNumCharts = pindanNum.toCharArray();
+        if (order.getType().equals("car")) {
+            if (count == 5) {
+                pindanNumCharts[0] = '0';
+            } else {
+                int num = Integer.parseInt(String.valueOf(scenic.getPindanNum().charAt(0))) + 1;
+                pindanNumCharts[0] = (char) (num + '0');
+            }
+        } else if (order.getType().equals("train")) {
+            if (count == 5) {
+                pindanNumCharts[1] = '0';
+            } else {
+                int num = Integer.parseInt(String.valueOf(scenic.getPindanNum().charAt(0))) + 1;
+                pindanNumCharts[1] = (char) (num + '0');
+            }
+        } else {
+            if (count == 5) {
+                pindanNumCharts[2] = '0';
+            } else {
+                int num = Integer.parseInt(String.valueOf(scenic.getPindanNum().charAt(0))) + 1;
+                pindanNumCharts[2] = (char) (num + '0');
+            }
+        }
+        scenic.setPindanNum(String.valueOf(pindanNumCharts));
+        scenicService.updateById(scenic);
 
         return new R();
     }
--- src/main/java/com/bishe/travel/entity/Order.java
+++ src/main/java/com/bishe/travel/entity/Order.java
@@ -17,6 +17,9 @@ public class Order {
 
     private int scenicId;
     private int userId;
+    private int status;
+    private String type;
+
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createDate; //创建时间
@@ -27,8 +30,26 @@ public class Order {
                 "id=" + id +
                 ", scenicId=" + scenicId +
                 ", userId=" + userId +
+                ", status=" + status +
+                ", type='" + type + '\'' +
                 ", createDate=" + createDate +
                 '}';
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
     }
 
     public int getId() {
